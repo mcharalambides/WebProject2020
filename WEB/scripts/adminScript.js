b75ed2061c244365119c21d142e17c3b95e3517b
MariosCh <mcharalampidis@ceid.upatras.gr>
2020-09-20 12:53:10 +0300

    20/09/20_part2

--- WEB/scripts/adminScript.js
+++ WEB/scripts/adminScript.js
@@ -401,17 +401,3 @@ $("#dataDelete").on("click",function(){
       }
 })
 
-
-
-
-$(document).ready(function () {
-   $("#confirm").click(function () {
-       $.ajax({
-               type: "POST",
-               url: "../php/insert.php",
-           })
-           .done(function (msg) {
-               alert("Data Saved: " + msg);
-           });
-   });
-});
